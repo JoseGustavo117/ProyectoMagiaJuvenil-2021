68356391b4e328f39dbde9eeb0577e5eaa3b19a6
JoseGustavo117 <80550749+JoseGustavo117@users.noreply.github.com>
2021-05-21 21:43:41 -0500

    Links de las imágenes
    Archivo con el link de las imágenes iniciales (en versiones posteriores se pueden añadir más imágenes)

---
+++ links.js
@@ -0,0 +1,7 @@
+//https://estaticos.elperiodico.com/resources/jpg/0/4/recepcion-hotel-barcelona-1484901441340.jpg
+//https://www.infoturperu.com.pe/media/k2/items/cache/6f393c73a9c9a064c4c50137fe9679b4_XL.jpg
+//http://elempresario.mx/sites/default/files/imagecache/nota_completa/60406011.jpg
+//https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2017/08/giraffe-manor-dining.jpg
+//https://st2.depositphotos.com/4061757/10892/i/950/depositphotos_108921206-stock-photo-man-and-woman-with-suitcases.jpg
+//http://www.planetacurioso.com/wp-content/uploads/2012/08/hotel-perros-nueva-york3.jpg
+//https://media-cdn.tripadvisor.com/media/photo-s/07/db/1b/92/hotel-lobby-and-the-show.jpg
